82d8d4d736f820691061cff3deaebbbfd50331e0
Matthieu de Goys <mdegoys+github@gmail.com>
2019-11-30 16:01:04 +0100

    Add first structure

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let stations = [
+    "Saint O'Lazare",
+"MADEleine",
+"P'YAMAdes",
+"ChâTOPI",
+"Gare d'ADZO",
+"BEMOrcy",
+"Cour St ZEMAlion",
+"BNFMI",
+"VAALympiade"]
+
+
   return (
     <div className="App">
       <h1>C'est la grève ? Prenez la 14 !</h1>
+    {stations.map(station => { return <p>{station}</p> })}
     </div>
   );
 }
